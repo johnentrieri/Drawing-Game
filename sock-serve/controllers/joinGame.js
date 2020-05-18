b201019f8cf1fd776b5d087b4f8e47321a93a5e0
DESKTOP-ECAGOK7\johnentrieri <john.entrieri@gmail.com>
2020-05-18 17:08:58 -0400

    Ported JoinGame to JS

---
+++ sock-serve/controllers/joinGame.js
@@ -0,0 +1,92 @@
+const Game = require('../db/models/gameSchema');
+
+joinGame = (request,response) => {   
+    const body = request.body;
+
+    //Check for Empty Body
+    if (!body) {
+        return response.status(400).json({
+            status: 'FAIL',
+            message: 'Body Empty'
+        })
+    }
+
+    //Check for Empty Room Name
+    if (!body.room) {
+        return response.status(400).json({
+            status: 'FAIL',
+            message: 'Room Name Empty'
+        })
+    }
+
+    //Check for Empty User Name
+    if (!body.user) {
+        return response.status(400).json({
+            status: 'FAIL',
+            message: 'User Name Empty'
+        })
+    }
+
+    Game.findOne( {name: body.room}, (err, doc) => {
+        if (err) {
+            return response.status(400).json({
+                status: 'FAIL',
+                message: 'Error Checking Database for Room'
+            })
+        } 
+        
+        //Check if Room Exists
+        if (!doc) {
+            return response.status(400).json({
+                status: 'FAIL',
+                message: 'Room Not Found'
+            })
+        } else {
+
+            //Check if Password is Correct
+            if (doc.code !== body.code) {
+                return response.status(400).json({
+                    status: 'FAIL',
+                    message: 'Room Code Incorrect'
+                })
+            } 
+
+            //Check if User is already in this Game
+            const teamList = Object.keys(doc.teams);
+            for (let i=0;i<teamList.length;i++) {
+                const playerList = doc.teams[teamList[i]].players;
+                if (playerList && playerList.includes(body.user)) {
+                    return response.status(200).json({
+                        status: 'SUCCESS',
+                        message: 'Rejoined Game',
+                    })
+                }
+            }
+
+            if (doc.state !== 'lobby') {
+                return response.status(400).json({
+                    status: 'FAIL',
+                    message: 'Game has already started',
+                })
+            } else {
+                
+                doc.teams.team1.players.push(body.user);
+                doc.save( (err) => {
+                    if (err) {
+                        return response.status(200).json({
+                            status: 'SUCCESS',
+                            message: 'Joined Game',
+                        })
+                    } else {
+                        return response.status(200).json({
+                            status: 'SUCCESS',
+                            message: 'Joined Game',
+                        })
+                    }
+                }) 
+            }
+        }
+    });
+};
+
+module.exports = joinGame;
--- sock-serve/routes/router.js
+++ sock-serve/routes/router.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 //Controllers
 const hostGame = require('../controllers/hostGame');
+const joinGame = require('../controllers/joinGame');
 
 //Router
 const router = express.Router()
 
 //API Routes
 router.post('/hostgame/', hostGame);
+router.post('/joingame/', joinGame);
 
 
 module.exports = router
